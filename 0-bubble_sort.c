52f65e2969f2af031f3efb8fe63e9dab5559ead2
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-05 11:32:01 +0000

    Added task 0 solution

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * bubble_sort - sort an array using bubble sort algorithm
+ * @array: array to be sort
+ * @size: length of the array
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j;
+
+	if (!array || !size)
+		return;
+
+	while (i < size)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+
+		i++;
+	}
+}
