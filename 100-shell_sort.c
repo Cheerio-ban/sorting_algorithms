23e460066b5e46d4af3635b9be13b810afcfa7c6
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-07 08:18:06 +0000

    Added shell sort implementation

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include "sort.h"
 /**
@@ -21,3 +22,37 @@ void shell_sort(int *array, size_t size)
 				array[j] = array[j - gap];
 		}
 
+=======
+#include "sort.h"
+#include <stddef.h>
+
+
+
+void shell_sort(int *arr, size_t size)
+{
+	size_t gap;
+
+	for (gap = size / 2; gap > 0; gap /= 2)
+	{
+		size_t i = gap;
+
+		while (i < size)
+		{
+			size_t j = i;
+			int tmp = arr[i];
+
+			while (j >= gap && arr[j - gap] > tmp)
+			{
+				arr[j] = arr[j - gap];
+
+				j -= gap;
+			}
+
+			arr[j] = tmp;
+			print_array(arr, size);
+
+			i++;
+		}
+	}
+}
+>>>>>>> dae4f38 (Added the rightful time complexity for bubble sort)
--- sort.h
+++ sort.h
@@ -23,6 +23,10 @@ void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+<<<<<<< HEAD
 void counting_sort(int *array, size_t size);
+=======
+void shell_sort(int *array, size_t size);
+>>>>>>> dae4f38 (Added the rightful time complexity for bubble sort)
 
 #endif
