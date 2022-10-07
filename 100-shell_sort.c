d22b5c68b42994b0459b902b5bb2fcc1ff252500
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-07 08:15:58 +0000

    Added shell sort implementation

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -27,32 +27,43 @@ void shell_sort(int *array, size_t size)
 #include <stddef.h>
 
 
+/**
+ * shell_sort - sort an array using shell sort algorithm with knuth sequence
+ * @arr: array to be sorted
+ * @size: length of the array
+ * Return: Nothing
+ */
+
 
 void shell_sort(int *arr, size_t size)
 {
-	size_t gap;
+	size_t gap = 1;
 
-	for (gap = size / 2; gap > 0; gap /= 2)
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
 	{
 		size_t i = gap;
 
 		while (i < size)
 		{
 			size_t j = i;
-			int tmp = arr[i];
 
-			while (j >= gap && arr[j - gap] > tmp)
+			while (j >= gap && arr[j - gap] > arr[j])
 			{
+				int tmp = arr[j];
+
 				arr[j] = arr[j - gap];
+				arr[j - gap] = tmp;
 
 				j -= gap;
 			}
 
-			arr[j] = tmp;
-			print_array(arr, size);
-
 			i++;
 		}
+
+		print_array(arr, size);
 	}
 }
 >>>>>>> dae4f38 (Added the rightful time complexity for bubble sort)
